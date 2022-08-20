db3e47bc191e4d26f40ae0ff35feb924a460a6ad
Kyaw Thura <44088165+kyawthura-gg@users.noreply.github.com>
2022-08-20 11:32:21 +0630

    chore: setup nativewind

--- App.tsx
+++ App.tsx
@@ -1,18 +1,11 @@
-import { StyleSheet, Text, View } from "react-native";
+import { Text, View } from "react-native";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app 11!</Text>
+    <View className="bg-black flex-1 justify-center items-center">
+      <Text className="text-white">
+        Open up App.tsx to start working on your app 11!
+      </Text>
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
