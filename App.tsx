b4be29a708d98a6eebd8abc18467ba541fcc532a
Kyaw Thura <44088165+kyawthura-gg@users.noreply.github.com>
2022-11-07 21:48:19 +0800

    fea: added in app browser

--- App.tsx
+++ App.tsx
@@ -1,10 +1,28 @@
 import { Amplify } from "aws-amplify";
 import awsconfig from "./src/aws-exports";
 import { SafeAreaView } from "react-native-safe-area-context";
+import * as WebBrowser from "expo-web-browser";
 import { MainNavigator } from "./src/navigators";
 import { AuthContextProvider } from "./src/contexts/auth-context";
+import { Linking, Platform } from "react-native";
 
-Amplify.configure(awsconfig);
+async function urlOpener(url: string, redirectUrl: string) {
+  const result = await WebBrowser.openAuthSessionAsync(url, redirectUrl);
+
+  if (result.type === "success" && Platform.OS === "ios") {
+    WebBrowser.dismissBrowser();
+    return Linking.openURL(result.url);
+  }
+}
+
+Amplify.configure({
+  ...awsconfig,
+  oauth: {
+    ...awsconfig.oauth,
+    urlOpener,
+  },
+});
+
 function App() {
   return (
     <SafeAreaView className="flex-1">
