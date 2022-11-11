da6db464930d9a82a89254c7a779fb3cceba1d23
Kyaw Thura <44088165+kyawthura-gg@users.noreply.github.com>
2022-11-11 17:27:37 +0800

    fix: added on user create trigger

--- amplify/backend/function/InstagramClonePostConfirmation/src/custom.js
+++ amplify/backend/function/InstagramClonePostConfirmation/src/custom.js
@@ -1,9 +1,71 @@
 /**
  * @type {import('@types/aws-lambda').APIGatewayProxyHandler}
  */
+const AWS = require("aws-sdk");
+const documentClient = new AWS.DynamoDB.DocumentClient();
+
+const env = process.env.ENV;
+const AppsyncID = process.env.API_INSTAGRAMCLONE_GRAPHQLAPIIDOUTPUT;
+const TableName = `User-${AppsyncID}-${env}`;
+
+const userExists = async (id) => {
+  const params = {
+    TableName,
+    Key: id,
+  };
+  try {
+    const response = await documentClient.get(params).promise();
+    return !!response?.Item;
+  } catch (error) {
+    return false;
+  }
+};
+const saveUser = async (user) => {
+  const date = new Date();
+  const dateStr = date.toISOString();
+  const timestamp = date.getTime();
+
+  const Item = {
+    ...user,
+    __typename: "User",
+    createdAt: dateStr,
+    updatedAt: dateStr,
+    _lastChangedAt: timestamp,
+    _version: 1,
+  };
+  const params = {
+    TableName,
+    Item,
+  };
+
+  try {
+    await documentClient.put(params).promise();
+    return !!response;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
 exports.handler = async (event, context) => {
   console.log("Lamdba here");
   console.log(event);
+  if (!event?.request?.userAttributes) {
+    console.log("No user!");
+    return;
+  }
+  const userAttributes = event.request.userAttributes;
+  const newUser = {
+    id: userAttributes?.sub,
+    name: userAttributes?.name,
+    email: userAttributes?.email,
+  };
+  const isExist = await userExists(newUser.id);
+  if (!isExist) {
+    console.log("user Exist");
+    await saveUser(newUser);
+  } else {
+    console.log("user not Exist!");
+  }
   // insert code to be executed by your lambda trigger
   return event;
 };
--- amplify/backend/function/InstagramClonePostConfirmation/src/index.js
+++ amplify/backend/function/InstagramClonePostConfirmation/src/index.js
@@ -1,4 +1,10 @@
-/**
+/* Amplify Params - DO NOT EDIT
+	API_INSTAGRAMCLONE_GRAPHQLAPIENDPOINTOUTPUT
+	API_INSTAGRAMCLONE_GRAPHQLAPIIDOUTPUT
+	API_INSTAGRAMCLONE_GRAPHQLAPIKEYOUTPUT
+	ENV
+	REGION
+Amplify Params - DO NOT EDIT *//**
  * @fileoverview
  *
  * This CloudFormation Trigger creates a handler which awaits the other handlers
